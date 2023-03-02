712451b6e77211a89c7bec6ee7061796e023006d
Paulo Henrique M de Morais <111690624+MarshallLiu-Dev@users.noreply.github.com>
2023-03-02 19:20:23 -0300

    second commit

--- heart.js
+++ heart.js
@@ -1,19 +1,4 @@
-/* heart in html and css written I love you you're the best 
-var heart = document.createElement('div');
-heart.innerHTML = '<div class="heart">&hearts;</div>';
-document.body.appendChild(heart);
-var style = document.createElement('style');
-style.innerHTML = '.heart { font-size: 100px; color: red; }';
-document.body.appendChild(style);
-var text = document.createElement('div');
-text.innerHTML = 'I love you you\'re the best';
-document.body.appendChild(text);
-
-
-heart */ 
-
-
-
+/* heart */ 
 // add a click event listener to the heart element
 
 document.getElementById("heart").addEventListener("click", function() {
